9364240e2cc3d286baa62c42946c7b9214c421b5
Himanshu Dixit <hudixt@gmail.com>
2024-04-09 22:53:26 +0530

    feat: load module

--- github_tool.py
+++ github_tool.py
@@ -1,15 +1,15 @@
 from shared.composio_tools.lib import Tool
-from .actions.create_issue import CreateIssue
-from .actions.list_repos import ListGithubRepos
-from .actions.about_me import GetAboutMe
-from .actions.star_repo import StarRepo
-from .actions.get_commits import GetCommits
-from .actions.get_commits_code import GetCommitsWithCode
-from .actions.create_repo_webhook import CreateRepoWebhook
-from .actions.fetch_readme import FetchReadme
-from .triggers.pull_request_event import PullRequestEvent
-from .triggers.commit_event import CommitEvent
-from .actions.get_patch_for_commit import GetPatchForCommit
+from my_custom.actions.create_issue import CreateIssue
+from my_custom.actions.list_repos import ListGithubRepos
+from my_custom.actions.about_me import GetAboutMe
+from my_custom.actions.star_repo import StarRepo
+from my_custom.actions.get_commits import GetCommits
+from my_custom.actions.get_commits_code import GetCommitsWithCode
+from my_custom.actions.create_repo_webhook import CreateRepoWebhook
+from my_custom.actions.fetch_readme import FetchReadme
+from my_custom.triggers.pull_request_event import PullRequestEvent
+from my_custom.triggers.commit_event import CommitEvent
+from my_custom.actions.get_patch_for_commit import GetPatchForCommit
 
 
 class Github(Tool):
