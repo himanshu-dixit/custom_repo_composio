a20e935da9744af0cbf33f7231c0e1c5dc4987eb
Himanshu Dixit <hudixt@gmail.com>
2024-04-09 22:00:02 +0530

    Update github_tool.py

--- github_tool.py
+++ github_tool.py
@@ -1,15 +1,15 @@
 from shared.composio_tools.lib import Tool
-from .actions.create_issue import CreateIssue
-from .actions.list_repos import ListGithubRepos
-from .actions.about_me import GetAboutMe
-from .actions.star_repo import StarRepo
-from .actions.get_commits import GetCommits
-from .actions.get_commits_code import GetCommitsWithCode
-from .actions.create_repo_webhook import CreateRepoWebhook
-from .actions.fetch_readme import FetchReadme
-from .triggers.pull_request_event import PullRequestEvent
-from .triggers.commit_event import CommitEvent
-from .actions.get_patch_for_commit import GetPatchForCommit
+from actions.create_issue import CreateIssue
+from actions.list_repos import ListGithubRepos
+from actions.about_me import GetAboutMe
+from actions.star_repo import StarRepo
+from actions.get_commits import GetCommits
+from actions.get_commits_code import GetCommitsWithCode
+from actions.create_repo_webhook import CreateRepoWebhook
+from actions.fetch_readme import FetchReadme
+from triggers.pull_request_event import PullRequestEvent
+from triggers.commit_event import CommitEvent
+from actions.get_patch_for_commit import GetPatchForCommit
 
 
 class Github(Tool):
